7a6ed6d879679d0c85901a45cbf82fb264ac8908
Rich Campbell <rich.campbell609@gmail.com>
2015-02-25 15:31:38 -0500

    Vector Math Homework

---
+++ Assets/script/Distance.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Distance : MonoBehaviour {
+
+	public GameObject endpoint;
+	public GameObject player;
+	public Text myText; 
+	bool hasDog = false;
+
+
+
+	void Update () {
+		float dist = Vector3.Distance(player.transform.position, endpoint.transform.position); 
+		Debug.Log (dist);
+
+		if(dist >= 200f){
+			myText.text = "You must find your mate!";
+		}
+		else if (dist >= 115f){
+			myText.text = "You hear your mate\nsoftly moaning in the distance\nyou are getting closer";
+		}
+		else if (dist >= 75f){
+			myText.text = "You hear your mate\nhowling, panting in excitment\nyou are close!";
+		}
+		else if (dist <= 42f){
+			myText.text = "Press [SPACEBAR] to beam your mate up!";
+			if(Input.GetKeyDown(KeyCode.Space)){
+				hasDog = true;
+			}
+		}
+		if(hasDog == true){
+			myText.text = "You have been reunited with your mate!\nYou WIN!";
+
+		}
+	}
+}
---
+++ Assets/script/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+	
+	public float speed = 5f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W)){
+			transform.position += transform.forward *Time.deltaTime * speed;
+		}
+		if (Input.GetKey (KeyCode.S)){
+			transform.position -= transform.forward *Time.deltaTime * speed;
+		}
+		
+		if (Input.GetKey(KeyCode.A)){
+			transform.Rotate (0f, -90f *Time.deltaTime, 0f); 
+		}
+		if (Input.GetKey(KeyCode.D)){
+			transform.Rotate (0f, 90f *Time.deltaTime, 0f); 
+		}
+	}
+}
